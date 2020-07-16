d1ddcb4de49cca5c47866d92a6fb7ef1bd6c06e3
jgcohetero <cohetero745@gmail.com>
2020-07-16 13:36:43 -0500

    registrar empresa load y ver empresa desde base de datos

--- src/app/pages/registrar/registrar.page.ts
+++ src/app/pages/registrar/registrar.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ConeccionService } from 'src/app/services/coneccion.service';
 import { NgModule } from '@angular/core';
+import { AlertController} from '@ionic/angular';
+import { LoadingController} from '@ionic/angular';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 @Component({
   selector: 'app-registrar',
@@ -18,13 +20,61 @@ export class RegistrarPage implements OnInit {
     telefono:"",
     direccion:"",
 };
-  constructor(private registrarEmpresa: ConeccionService, private empresaForm: FormBuilder) { }
+  constructor(private registrarEmpresa: ConeccionService, private empresaForm: FormBuilder, private cargarCtrl: LoadingController, public alertaCtrl: AlertController) { }
 
   ngOnInit() {
   }
-  registrar() {
-    this.registrarEmpresa.registrarEmpresas(this.empresa)
-    console.log(this.empresa);
+  
+  async registrar() {
+    this.registrarEmpresa.registrarEmpresas(this.empresa).then(()=>{
+      this.cargar();
+      console.log('Empresa registrada');
+    },
+    (err)=>{
+      console.log(err)
+    })
+    this.alerta();
+    this.limpiarDatos();
   }
+  async cargar(){
+    let loading= await this.cargarCtrl.create({
+      message: 'Cargando...'
+    });
+    loading.present();
+    setTimeout(()=>{
+      loading.dismiss();
+      
+    },2000)
+  }
+  async alerta(){
+    let alert= await this.alertaCtrl.create({
+      header: 'Registrado',
+      message: 'Se ha registrado con éxito',
+      buttons:[
+        {
+          text: 'OK',
+          handler:() => {
 
+          }
+        }
+      ]
+    });
+    setTimeout(()=>{
+      alert.present();
+    },2000)
+    alert.dismiss();
+  }
+  
+  limpiarDatos(){
+    this.empresa ={
+      id:'',
+      nombre: '',
+      noInterior:"",
+      noExterior:"",
+      noLamparas:"",
+      contacto:"",
+      telefono:"",
+      direccion:"",
+  };
+  }
 }
