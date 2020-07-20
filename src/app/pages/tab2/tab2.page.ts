193cb7bb48c6d498a6682032169d753fd08c6412
Marvend <marvin.aguilar@hotmail.com>
2020-07-19 20:02:36 -0500

    graficas al 90%

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -7,49 +7,44 @@ import { Chart } from 'chart.js';
 })
 export class Tab2Page implements OnInit {
   @ViewChild('barChart', { static: true }) barChart;
+  @ViewChild('barChart2', { static: true }) barChart2;
   bars: any;
+  bars2: any;
   constructor() { }
 
   ngOnInit() {
-    this.createBarChart();
+    this.GraficaInteriores();
+    this.GraficaExteriores();
   }
 
-  createBarChart() {
+  GraficaInteriores() {
     this.bars = new Chart(this.barChart.nativeElement, {
       type: 'bar',
-    data: {
-      labels: ['arañas', 'tijerillas', 'araña', 'otros'],
-        datasets: [{
-            label: 'Trampas',
-            data: [2, 1, 3, 4],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+      data: {
+        labels: ["1", "2", "3", "4", "5", "6", "7", "8"],
+  datasets: [{
+    label: 'Consumo de Trampas Interiores',
+    data: [2, 0, 5, 11, 0, 3, 0, 1],
+    backgroundColor: 'rgba(0,187,100)'
+  }]
+      }
     });
   }
+
+  GraficaExteriores() {
+    this.bars2 = new Chart(this.barChart2.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ["1", "2", "3", "4", "5", "6", "7", "8"],
+  datasets: [{
+    label: 'Consumo de Trampas Exteriores',
+    data: [0, 100, 50, 0, 100, 50, 0, 100],
+    backgroundColor: 'rgba(0,187,100)'
+  }]
+      }
+    });
+  }
+
+
+
 }
