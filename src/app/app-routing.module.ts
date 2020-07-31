dca4c3faab96d78206020aab54f4bd9b3de854a5
jgcohetero <cohetero745@gmail.com>
2020-07-30 20:25:47 -0500

    grafica y splash nuevo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,51 +2,48 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule'},
+  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
   {
     path: 'registrar',
-    loadChildren: () => import('./pages/registrar/registrar.module').then( m => m.RegistrarPageModule)
+    loadChildren: () => import('./pages/registrar/registrar.module').then(m => m.RegistrarPageModule)
   },
   {
     path: 'analizar',
-    loadChildren: () => import('./pages/analizar/analizar.module').then( m => m.AnalizarPageModule)
+    loadChildren: () => import('./pages/analizar/analizar.module').then(m => m.AnalizarPageModule)
   },
   {
     path: 'empresa/:id',
-    loadChildren: () => import('./pages/empresa/empresa.module').then( m => m.EmpresaPageModule)
+    loadChildren: () => import('./pages/empresa/empresa.module').then(m => m.EmpresaPageModule)
   },
   {
     path: 'exteriores/:id',
-    loadChildren: () => import('./pages/exteriores/exteriores.module').then( m => m.ExterioresPageModule)
+    loadChildren: () => import('./pages/exteriores/exteriores.module').then(m => m.ExterioresPageModule)
   },
   {
     path: 'interior/:id',
-    loadChildren: () => import('./pages/interior/interior.module').then( m => m.InteriorPageModule)
+    loadChildren: () => import('./pages/interior/interior.module').then(m => m.InteriorPageModule)
   },
   {
     path: 'lamparas/:id',
-    loadChildren: () => import('./pages/lamparas/lamparas.module').then( m => m.LamparasPageModule)
+    loadChildren: () => import('./pages/lamparas/lamparas.module').then(m => m.LamparasPageModule)
   },
   {
     path: 'portada/:id',
-    loadChildren: () => import('./pages/portada/portada.module').then( m => m.PortadaPageModule)
+    loadChildren: () => import('./pages/portada/portada.module').then(m => m.PortadaPageModule)
   },
   {
-<<<<<<< HEAD
     path: 'graficas',
-    loadChildren: () => import('./pages/graficas/graficas.module').then( m => m.GraficasPageModule)
+    loadChildren: () => import('./pages/graficas/graficas.module').then(m => m.GraficasPageModule)
   },
   {
     path: 'graficas/:id',
-    loadChildren: () => import('./pages/graficas/graficas.module').then( m => m.GraficasPageModule)
+    loadChildren: () => import('./pages/graficas/graficas.module').then(m => m.GraficasPageModule)
   },
-
-
-=======
+  {
     path: 'reportes/:id',
-    loadChildren: () => import('./pages/reportes/reportes.module').then( m => m.ReportesPageModule)
+    loadChildren: () => import('./pages/reportes/reportes.module').then(m => m.ReportesPageModule)
   }
->>>>>>> 0e11ea8151bc889038218908237324dec0c5f96b
+
 ];
 
 @NgModule({
